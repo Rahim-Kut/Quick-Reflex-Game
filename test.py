730ee12b83a13a0fb883f37b82e77c43492216ec
skarkii <lucas@kuja-halkola.com>
2025-05-13 10:57:50 +0100

    Revert "Merge branch 'main' of https://github.com/Rahim-Kut/Quick-Reflex-Game"
    This reverts commit e917602772c225bd7adec34f7dcf132f8c0ea2fc, reversing
    changes made to ad7d117089afcc4233db1784f44f93800c217ffa.

---
+++ test.py
@@ -0,0 +1,28 @@
+import explorerhat
+import time
+
+# Turn buzzer on (Output 1)
+# print("Turning buzzer On...")
+# explorerhat.output.one.on()
+# time.sleep(1)
+
+# Turn it off
+# print("Turning buzzer off")
+# explorerhat.output.one.off()
+
+def beam_broken(channel):
+	print("Beam broken!")
+	explorerhat.output.one.on()
+	time.sleep(0.2)
+	explorerhat.output.one.off()
+	
+explorerhat.input.one.changed(beam_broken)
+
+print("Ready. Break the beam to trigger the buzzer.")
+
+
+#print("Reading input 1 every 0.5s...")
+
+while True:
+	#print("Input 1:", explorerhat.input.one.read())
+	time.sleep(1)
